1cfe48c9467ea1d7574725400274788d76b43aac
elizabeth sky degrate <skydegrate@gmail.com>
2022-07-30 10:11:40 -0400

    denmo nagatoro

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  // test code for binding two indentifies to two buttons
+  // it works
+  let allForms = document.getElementsByTagName('form')
+  for (let i = 0; i < allForms.length; i++) {
+    allForms[i].addEventListener('submit' , (e) => {
+    e.preventDefault()
+    buildMiamiInput(e.target["new-task-description"].value)
+    form.reset()
+  })
+}});
+
+function buildMiamiInput(input){
+  let p = document.createElement('p') 
+  let button = document.createElement('button')
+  button.addEventListener('click', handleDelete)
+  button.textContent = " click to delete!"
+  p.textContent = `${input}  ` 
+  p.appendChild(button)
+  console.log(p)
+  document.querySelector("#list").appendChild(p);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+} 
